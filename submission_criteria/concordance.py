eb09d5a1d407a34674a4009f4b8739d95a42ba49
Anson Chu <anson@numer.ai>
2018-11-15 11:15:47 -0800

    fix pylint issues

--- submission_criteria/concordance.py
+++ submission_criteria/concordance.py
@@ -11,7 +11,7 @@ import numpy as np
 import pandas as pd
 
 # First Party
-import submission_criteria.common as common
+from submission_criteria import common
 
 
 def has_concordance(P1, P2, P3, c1, c2, c3, threshold=0.12):
